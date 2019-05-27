43ff733f625e70b4ee3460a32ebda4249aee901f
mty2470 <1306533679@qq.com>
2019-05-27 20:39:00 +0800

    静态页面构建

---
+++ vue.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+// vue 项目脚手架执行的配置
+module.exports = {
+//   devServer: {
+//     // 配置本地服务器的正向代理（开发环境）
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:9000",
+//         // target: "http://192.168.137.1:9090",
+//         changeOrigin: true
+//       }
+//     }
+//   },
+
+  // 进行 webpack 配置
+  chainWebpack: config => {
+    config.resolve.alias
+    //   .set("API", resolve("src/api"))
+    //   .set("fetch", resolve("src/api/fetch"))
+      .set("@views", resolve("src/views"))
+      .set("@components", resolve("src/components"));
+  }
+}
