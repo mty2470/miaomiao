c4f05ac3e4320663cbde18ef132c18ef7e1d3069
mty2470 <1306533679@qq.com>
2019-06-02 21:15:31 +0800

    个人组件划分

--- src/routers/minerouter/index.js
+++ src/routers/minerouter/index.js
@@ -1,4 +1,26 @@
 export default {
     path:"/mine",
-    component:()=>import("@views/Mine")
+    component:()=>import("@views/Mine"),
+    children:[
+        {
+            path:'personal',
+            component:()=>import('@views/Mine/personal.vue')      
+        },
+        {
+            path:'login',
+            component:()=>import('@components/Login')
+        },
+        {
+            path:'register',
+            component:()=>import('@components/Register')
+        },
+        {
+            path:'updatepassword',
+            component:()=>import('@components/updatePassword')
+        },
+        {
+            path:'/mine',
+            redirect:'personal' //重定向到个人中心
+        }
+    ]
 }
